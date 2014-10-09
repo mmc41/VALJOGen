5398fa6a268cdead1085950a6a09b37a8ff04988
Morten Christensen <mmc@41concepts.com>
2014-10-09 14:37:57 +0200

    updated website

--- deploy_website.sh
+++ deploy_website.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 set -x
 cp -R site/output/ .
+git status
+git add -u
+git add apidocs
+git add *.html
+git add images
+git add js
+git add css
+git add fonts
+git commit -m "updated website"
+git push origin gh-pages
