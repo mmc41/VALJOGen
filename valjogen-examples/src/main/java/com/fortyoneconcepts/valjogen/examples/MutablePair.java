4fc7b941f4f16ef1a83936d9b860bad021734008
Morten Christensen <mmc@41concepts.com>
2014-11-09 17:34:47 +0100

    added explicit mutability for examples

--- valjogen-examples/src/main/java/com/fortyoneconcepts/valjogen/examples/MutablePair.java
+++ valjogen-examples/src/main/java/com/fortyoneconcepts/valjogen/examples/MutablePair.java
@@ -3,16 +3,22 @@
 */
 package com.fortyoneconcepts.valjogen.examples;
 
-import com.fortyoneconcepts.valjogen.annotations.VALJOGenerate;
+import com.fortyoneconcepts.valjogen.annotations.*;
+import com.fortyoneconcepts.valjogen.annotations.types.Mutability;
 
 /**
 * Example that shows how to generate a simple mutable pair class.
 * Note the tiny difference compared to the immutable pair class example.
 *
+* The extra @VALJOConfigure annotation specifying mutability is optional,
+* in this example but is can be a good idea to explictly specify it
+* to get compile time verification etc.
+*
 * @param <TLeft> First object type in pair.
 * @param <TRight> Second object type in pair.
 */
 @VALJOGenerate(comment="Example 3")
+@VALJOConfigure(mutability=Mutability.Mutable)
 public interface MutablePair<TLeft, TRight>
 {
 	public TLeft getLeft();
